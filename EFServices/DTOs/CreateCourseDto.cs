4ab2ef4bb175356be765ce97fdea9fb847e1a228
Lalit Chauhan <lalit.chauhan@venture7.com>
2025-05-05 21:55:10 +0530

    added validation and update deleteAt

--- EFServices/DTOs/CreateCourseDto.cs
+++ EFServices/DTOs/CreateCourseDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,12 @@ namespace EFServices.DTOs
 {
     public class CreateCourseDto
     {
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, ErrorMessage = "Title can't be longer than 100 characters.")]
         public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = "Credits are required.")]
+        [Range(0, 100, ErrorMessage = "Credits must be between 0 and 100.")]
         public int Credits { get; set; }
     }
 }
--- EFServices/DTOs/CreateStudentDto.cs
+++ EFServices/DTOs/CreateStudentDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace EFServices.DTOs
 {
     public class CreateStudentDto
     {
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(50, ErrorMessage = "First name can't be longer than 50 characters.")]
         public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
+
+        [StringLength(50, ErrorMessage = "Last name can't be longer than 50 characters.")]
+        public string? LastName { get; set; }
+
         public CreateStudentProfileDto? Profile { get; set; }
     }
 }
